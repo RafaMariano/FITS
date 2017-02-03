da5e82a7c1a1f947191ef173b7faca953ec16be6
Rafa Mariano <rafaelmonteiromariano@gmail.com>
2017-02-03 11:57:27 -0200

    Começo da paginaçao

--- SaveImage/src/br/inpe/filesystem/Main.java
+++ SaveImage/src/br/inpe/filesystem/Main.java
@@ -3,18 +3,22 @@ package br.inpe.filesystem;
 import java.io.IOException;
 import java.text.ParseException;
 
+import br.inpe.database.Query;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException, ParseException {
 		
-		try{
-			FileSystem f = new FileSystem();
-			f.sendToBD(f.getImages());
-		}
-		catch(IOException io){
-
-		}
-	
+//		try{
+//			FileSystem f = new FileSystem();
+//			f.sendToBD(f.getImages());
+//		}
+//		catch(IOException io){
+//
+//		}
+	//while (true){
+		Query.findOne("", 0);
+	//}
 	}
 
 }
